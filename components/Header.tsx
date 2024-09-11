e7eef6b56851fb9adb98a8d8c5406367bf370441
Libenson Nicolas <libenson.n@gmail.com>
2024-09-11 00:46:25 -0400

    removed unused variable declaration

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,6 @@ import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { checkUser } from "@/lib/checkUser";
 
 const Header = async () => {
-  const user = await checkUser();
-
   return (
     <nav className="flex h-14 items-center justify-between bg-violet-400 px-8 shadow-md">
       <h2 className="font-extrabold">BUDGET⋅TO</h2>
